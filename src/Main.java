c9a7186835652e73e53af7af92e0d37d161ad783
Nicole0509 <niashavril0509@gmail.com>
2025-09-19 15:35:58 +0200

    Created a function used to get the most Expensive Items

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,10 @@ import java.util.ArrayList;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("Welcome To Your Inventory Management System\n");
+    static ArrayList<Item> items = new ArrayList<>();
 
-        ArrayList<Item> items = new ArrayList<>();
-
-        items.add(new Item(1, "Lenovo", 10000, "Laptops"));
-        items.add(new Item(2,"HP", 3000,"Laptops"));
-
+    public static Item getMostExpensiveItems(ArrayList<Item> items) {
         double temporaryPrice = 0;
         Item expensiveItem = null;
 
@@ -20,7 +15,20 @@ public class Main {
             expensiveItem = item;
         }
 
-        System.out.println("The most expensive item is " + expensiveItem);
+        return expensiveItem;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome To Your Inventory Management System\n");
+
+        items.add(new Item(1, "Lenovo", 10000, "Laptops"));
+        items.add(new Item(2,"HP", 3000,"Laptops"));
+        items.add(new Item(3,"Samsung", 5000,"Smartphones"));
+        items.add(new Item(4,"Galaxy", 11000,"Smartphones"));
+
+        System.out.println("The most expensive item is " + getMostExpensiveItems(items));
 
 
 
