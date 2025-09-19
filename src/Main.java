6368aae3d4d2155b732c0dbb040622f6ded5e4c6
Nicole0509 <niashavril0509@gmail.com>
2025-09-19 14:30:00 +0200

    Successfully created a working array list and displayed items

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome To Your Inventory Management System\n");
+
+        ArrayList<String> items = new ArrayList<>();
+
+        items.add("Sword");
+        items.add("Axe");
+        items.add("Pickaxe");
+        items.add("Shovel");
+        items.add("Hoe");
+        items.add("Pickaxe");
+
+        System.out.println(items);
 
     }
 }
