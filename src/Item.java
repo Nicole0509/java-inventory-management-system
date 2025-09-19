1a6311013ba8ace87731b92789418a7804d242ed
Nicole0509 <niashavril0509@gmail.com>
2025-09-19 14:36:12 +0200

    Created the item class and put fields

---
+++ src/Item.java
@@ -0,0 +1,14 @@
+public class Item {
+    private int itemId;
+    private String itemName;
+    private double itemPrice;
+    private String itemCategory;
+
+    public Item(int itemId, String itemName, double itemPrice, String itemCategory) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.itemCategory = itemCategory;
+    }
+
+}
