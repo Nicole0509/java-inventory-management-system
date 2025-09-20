9b8fbaff2dd648f495f047425b9671c9814f653d
Nicole0509 <niashavril0509@gmail.com>
2025-09-20 12:39:32 +0200

    Created a switch Menu method for displaying the menu whenver needed

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,70 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
     static int choice;
 
+    public static int SwithMenu(int choice) {
+        int itemId;
+
+        System.out.println("\nMenu Options");
+        System.out.println("1. Get All Items");
+        System.out.println("2. Add Item");
+        System.out.println("3. Remove Item");
+        System.out.println("4. Get Most Expensive Item");
+        System.out.println("5. Exit");
+
+        System.out.print("\nPlease choose an option: ");
+        choice = scanner.nextInt();
+
+        switch(choice) {
+            case 1:
+                if(items.isEmpty()) {
+                    System.out.println("\nThere are no items in the inventory yet");
+                    System.out.println("Please add an item");
+                } else {
+                    System.out.println("\nA list of all items in the inventory");
+                    getAllItems(items);
+                }
+                break;
+            case 2:
+                System.out.println("\nPlease enter the item you want to add");
+                System.out.print("Enter the item ID: ");
+                itemId = scanner.nextInt();
+                System.out.print("Enter the item Name: ");
+                String itemName = scanner.nextLine();
+                scanner.nextLine();
+                System.out.print("Enter the item price: ");
+                double itemPrice = scanner.nextDouble();
+                System.out.print("Enter the item category: ");
+                String itemCategory = scanner.next();
+
+                addItem(items,itemId,itemName,itemPrice,itemCategory);
+
+                System.out.println("Item added successfully");
+                System.out.println("You now have " + items.size() + " item(s) in your inventory!");
+                break;
+            case 3:
+                itemId = scanner.nextInt();
+                removeItemById(items, itemId);
+                break;
+            case 4:
+                if(items.isEmpty()) {
+                    System.out.println("\nThere are no items in the inventory yet");
+                    System.out.println("Please add an item");
+                } else {
+                    System.out.println("\nThe most expensive item " + getMostExpensiveItems(items) );
+                }
+                break;
+            case 5:
+                System.out.println("\nExit");
+                System.out.println("The System Says Goodbye!");
+                break;
+            default:
+                System.out.println("\nInvalid choice");
+                break;
+        }
+
+        return choice;
+    }
+
     public static void addItem(ArrayList<Item> items, int itemId, String itemName, double itemPrice, String itemCategory ) {
         items.add(new Item(itemId,itemName, itemPrice,itemCategory));
     }
@@ -43,68 +107,12 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int itemId;
 
         System.out.println("Welcome To Your Inventory Management System\n");
 
-        System.out.println("Pick a choice and Manage Your Inventory");
-        System.out.println("1. Get All Items");
-        System.out.println("2. Add Item");
-        System.out.println("3. Remove Item");
-        System.out.println("4. Get Most Expensive Item");
-        System.out.println("5. Exit");
-
         do{
-            System.out.print("\nPlease choose an option: ");
-            choice = scanner.nextInt();
+            choice = SwithMenu(choice);
 
-            switch(choice) {
-                case 1:
-                    if(items.isEmpty()) {
-                        System.out.println("\nThere are no items in the inventory yet");
-                        System.out.println("Please add an item");
-                    } else {
-                        System.out.println("\nA list of all items in the inventory");
-                        getAllItems(items);
-                    }
-                    break;
-                case 2:
-                    System.out.println("\nPlease enter the item you want to add");
-                    System.out.print("Enter the item ID: ");
-                    itemId = scanner.nextInt();
-                    System.out.print("Enter the item Name: ");
-                    String itemName = scanner.nextLine();
-                    scanner.nextLine();
-                    System.out.print("Enter the item price: ");
-                    double itemPrice = scanner.nextDouble();
-                    System.out.print("Enter the item category: ");
-                    String itemCategory = scanner.next();
-
-                    addItem(items,itemId,itemName,itemPrice,itemCategory);
-
-                    System.out.println("Item added successfully");
-                    System.out.println("You now have " + items.size() + " item(s) in your inventory!");
-                    break;
-                case 3:
-                    itemId = scanner.nextInt();
-                    removeItemById(items, itemId);
-                    break;
-                case 4:
-                    if(items.isEmpty()) {
-                        System.out.println("\nThere are no items in the inventory yet");
-                        System.out.println("Please add an item");
-                    } else {
-                        System.out.println("\nThe most expensive item " + getMostExpensiveItems(items) );
-                    }
-                    break;
-                case 5:
-                    System.out.println("\nExit");
-                    System.out.println("The System Says Goodbye!");
-                    break;
-                default:
-                    System.out.println("\nInvalid choice");
-                    break;
-            }
         } while(choice != 5);
 
     }
